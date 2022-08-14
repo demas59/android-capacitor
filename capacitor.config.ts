96d4b4edc34ed4456b38320983739cd561c7a9be
demas59 <demasay@gmail.com>
2022-08-14 21:09:11 +0200

    update angular setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'android-capacitor',
+  webDir: 'dist/android-capacitor',
+  bundledWebRuntime: false,
+};
+
+export default config;
